bdfdf5ccd83b73bf4cd2197041815138fb2af44b
Patryk Mierzejewski <mierzej.pl@gmail.com>
2018-08-09 13:42:16 +0200

    initial commit

---
+++ Example/App.js
@@ -0,0 +1,31 @@
+import React, { Component } from "react";
+import { Platform, StyleSheet, View } from "react-native";
+import TabBar from "fluidbottomnavigation-rn";
+
+export default class App extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <View />
+        <TabBar
+          values={[
+            { title: "News", icon: require("./assets/news.png") },
+            { title: "Requests", icon: require("./assets/requests.png") },
+            { title: "Events", icon: require("./assets/events.png") },
+            { title: "Members", icon: require("./assets/members.png") },
+            { title: "Account", icon: require("./assets/account.png") }
+          ]}
+        />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "space-between",
+    alignItems: "center",
+    backgroundColor: "#4C53DD"
+  }
+});
---
+++ index.js
@@ -0,0 +1,241 @@
+import React, { Component } from "react";
+import {
+  TouchableWithoutFeedback,
+  Animated,
+  Easing,
+  Image
+} from "react-native";
+import ViewOverflow from "react-native-view-overflow";
+
+const AnimatedViewOverflow = Animated.createAnimatedComponent(ViewOverflow);
+
+class TabBar extends Component {
+  constructor(props) {
+    super(props);
+    this.animatedItemValues = [];
+    this.animatedBubbleValues = [];
+    this.animatedMiniBubbleValues = [];
+    this.animatedImageValues = [];
+    this.props.values.forEach((item, index) => {
+      this.animatedItemValues[index] = new Animated.Value(0);
+      this.animatedBubbleValues[index] = new Animated.Value(0);
+      this.animatedImageValues[index] = new Animated.Value(0);
+      this.animatedMiniBubbleValues[index] = new Animated.Value(0);
+    });
+  }
+
+  state = {
+    lastSelectedIndex: null
+  };
+
+  _renderButtons = () => {
+    return this.props.values.map((item, index) => {
+      const animatedItemStyle = {
+        transform: [{ translateY: this.animatedItemValues[index] }]
+      };
+
+      const animatedBubbleScaleValues = this.animatedBubbleValues[
+        index
+      ].interpolate({
+        inputRange: [0, 0.25, 0.4, 0.525, 0.8, 1],
+        outputRange: [0.01, 3, 1.65, 1.65, 3.2, 3]
+      });
+
+      const animatedColorValues = this.animatedImageValues[index].interpolate({
+        inputRange: [0, 1],
+        outputRange: ["rgb(76, 83, 221)", "rgb(255, 255, 255)"]
+      });
+
+      const animatedBubbleStyle = {
+        transform: [{ scale: animatedBubbleScaleValues }]
+      };
+
+      const animatedImageStyle = {
+        tintColor: animatedColorValues
+      };
+
+      const animatedMiniBubbleTranslateValues = this.animatedMiniBubbleValues[
+        index
+      ].interpolate({
+        inputRange: [0, 1],
+        outputRange: [13, 0]
+      });
+
+      const animatedMiniBubbleHeightValues = this.animatedMiniBubbleValues[
+        index
+      ].interpolate({
+        inputRange: [0, 0.01, 1],
+        outputRange: [0, 1, 1]
+      });
+
+      const animatedMiniBubbleStyle = {
+        opacity: animatedMiniBubbleHeightValues,
+        transform: [{ translateY: animatedMiniBubbleTranslateValues }]
+      };
+
+      const animatedTitleValues = this.animatedBubbleValues[index].interpolate({
+        inputRange: [0, 1],
+        outputRange: [60, 60]
+      });
+
+      const animatedTitleStyle = {
+        transform: [{ translateY: animatedTitleValues }]
+      };
+
+      return (
+        <TouchableWithoutFeedback
+          key={index}
+          onPress={() => {
+            if (index === this.state.lastSelectedIndex) {
+              return;
+            }
+
+            this.startAnimation(index);
+
+            if (this.state.lastSelectedIndex !== null) {
+              this.endAnimation(this.state.lastSelectedIndex);
+            }
+
+            this.setState({
+              lastSelectedIndex: index
+            });
+          }}
+        >
+          <AnimatedViewOverflow style={[styles.item, animatedItemStyle]}>
+            <Image
+              style={styles.itemMask}
+              source={require("../assets/czapka.png")}
+            />
+            <Animated.View style={[styles.bubble, animatedBubbleStyle]} />
+            <Animated.View style={[miniBubble, animatedMiniBubbleStyle]} />
+            <Animated.Image source={item.icon} style={animatedImageStyle} />
+            <Animated.View style={[styles.titleContainer, animatedTitleStyle]}>
+              <Animated.Text
+                numberOfLines={1}
+                adjustsFontSizeToFit={true}
+                style={{
+                  color: "#4C53DD"
+                }}
+              >
+                {item.title}
+              </Animated.Text>
+            </Animated.View>
+          </AnimatedViewOverflow>
+        </TouchableWithoutFeedback>
+      );
+    });
+  };
+
+  startAnimation = index => {
+    Animated.parallel([
+      Animated.timing(this.animatedItemValues[index], {
+        toValue: -30,
+        duration: 500,
+        delay: 300,
+        easing: Easing.in(Easing.elastic(1.5)),
+        useNativeDriver: true
+      }),
+      Animated.timing(this.animatedMiniBubbleValues[index], {
+        toValue: 1,
+        duration: 1000,
+        delay: 300,
+        useNativeDriver: true
+      }),
+      Animated.timing(this.animatedBubbleValues[index], {
+        toValue: 1,
+        duration: 800,
+        easing: Easing.inOut(Easing.out(Easing.ease)),
+        useNativeDriver: true
+      }),
+      Animated.timing(this.animatedImageValues[index], {
+        toValue: 1,
+        duration: 800
+      })
+    ]).start();
+  };
+
+  endAnimation = index => {
+    Animated.parallel([
+      Animated.timing(this.animatedItemValues[index], {
+        toValue: 0,
+        duration: 400,
+        delay: 350,
+        useNativeDriver: true
+      }),
+      Animated.timing(this.animatedMiniBubbleValues[index], {
+        toValue: 0,
+        duration: 1,
+        useNativeDriver: true
+      }),
+      Animated.timing(this.animatedBubbleValues[index], {
+        toValue: 0,
+        duration: 750,
+        easing: Easing.out(Easing.ease),
+        useNativeDriver: true
+      }),
+      Animated.timing(this.animatedImageValues[index], {
+        toValue: 0,
+        duration: 400,
+        delay: 350
+      })
+    ]).start();
+  };
+
+  render() {
+    return (
+      <AnimatedViewOverflow style={styles.container}>
+        {this._renderButtons()}
+      </AnimatedViewOverflow>
+    );
+  }
+}
+
+const styles = {
+  container: {
+    flexDirection: "row",
+    height: 60,
+    width: "100%",
+    justifyContent: "space-around",
+    alignItems: "center",
+    backgroundColor: "white"
+  },
+  item: {
+    backgroundColor: "transparent",
+    borderRadius: 30,
+    height: 60,
+    width: 60,
+    alignItems: "center",
+    justifyContent: "center"
+  },
+  itemMask: {
+    tintColor: "white",
+    position: "absolute"
+  },
+  bubble: {
+    position: "absolute",
+    alignSelf: "center",
+    height: 17,
+    width: 17,
+    backgroundColor: "#4C53DD",
+    borderRadius: 8.5
+  },
+  miniBubble: {
+    position: "absolute",
+    alignSelf: "center",
+    width: 22,
+    height: 22,
+    backgroundColor: "#4C53DD",
+    borderRadius: 11
+  },
+  titleContainer: {
+    position: "absolute",
+    top: 0,
+    left: 0,
+    right: 0,
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center"
+  }
+};
+
+export default TabBar;
